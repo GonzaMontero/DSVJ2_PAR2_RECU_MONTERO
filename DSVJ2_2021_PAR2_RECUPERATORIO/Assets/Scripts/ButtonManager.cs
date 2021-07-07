1b3cf718b59156c43b27c31cdbb8ce1ff858b3c9
Gonzalo Montero <gonmon654@gmail.com>
2021-07-07 18:25:01 -0300

    Added TMP Essentials and Added Basic Main Menu

---
+++ DSVJ2_2021_PAR2_RECUPERATORIO/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void PlayRestart()
+    {
+        SceneManager.LoadScene("Main Game");
+    }
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene("Settings and Credits");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
