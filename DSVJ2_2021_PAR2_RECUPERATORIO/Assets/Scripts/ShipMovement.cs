a5190b1ca7750277bb203631edc6239f80a318b2
Gonzalo Montero <gonmon654@gmail.com>
2021-07-07 17:58:36 -0300

    Added Sprite Shape Controller, Ship Primitive Movement and Sprite Terrain Shape

---
+++ DSVJ2_2021_PAR2_RECUPERATORIO/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D),typeof(BoxCollider2D))]
+public class ShipMovement : MonoBehaviour
+{
+    [SerializeField] float jumpForce;
+    [SerializeField] float rotateSpeed;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotateShip(rotateSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RotateShip(-rotateSpeed);
+        }
+    }
+
+    void RotateShip(float rotateSpeed)
+    {
+        transform.Rotate(new Vector3(0, 0, rotateSpeed * Time.deltaTime));
+    }
+}
