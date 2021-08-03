b8e57a5b23da2022415d93221163a42489a8f946
Gonzalo Montero <gonmon654@gmail.com>
2021-08-03 12:38:51 -0300

    Modified Meta

---
+++ DSVJ2_2021_PAR2_RECUPERATORIO/Assets/Scripts/CameraFollow (1).cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+namespace CameraFollowScript
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] public Transform lookAtThat;
+
+        [SerializeField] [Range(0.5f, 9)] public float speedFollow;
+        [SerializeField] [Range(30, 70)] public float maxAltitudToZoom;
+        [SerializeField] public bool followPlayer;
+        private float sizeCamera;
+        private float initialSizeCamera;
+
+        private Vector2 posToMoveTowards;
+        private Vector2 initialCameraPosition;
+
+        private ShipMovement player;
+
+        private void Awake()
+        {
+            player = lookAtThat.GetComponent<ShipMovement>();
+            initialCameraPosition = transform.position;
+            initialSizeCamera = Camera.main.orthographicSize;
+        }
+        public void LookAtPlayer()
+        {
+            transform.LookAt(lookAtThat, lookAtThat.up);
+        }
+        void LateUpdate()
+        {
+            if(player != null)
+            {
+                if (player.altitude < maxAltitudToZoom)
+                {
+                    followPlayer = true;
+                    posToMoveTowards = lookAtThat.position;
+                }
+                else
+                    followPlayer = false;
+            }
+
+            if (followPlayer)
+            {
+                FocusToTargetAndMove();
+            }
+            else
+            {
+                transform.position = Vector2.Lerp(transform.position, initialCameraPosition, Time.deltaTime * speedFollow);
+                Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, initialSizeCamera, Time.deltaTime * speedFollow);
+            }
+        }
+        public void FocusToTargetAndMove()
+        {
+            sizeCamera = initialSizeCamera/3;
+
+            if (lookAtThat != null)
+            {
+                posToMoveTowards = lookAtThat.position;
+                transform.position = Vector2.Lerp(transform.position, posToMoveTowards, Time.deltaTime * speedFollow);
+                Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, sizeCamera, Time.deltaTime * speedFollow);
+            }
+        }
+    }
+}
